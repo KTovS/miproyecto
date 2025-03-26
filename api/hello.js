cec62b9a8ec4442c87108cbea07f5a8215e6e3ac
KTovS <kenia.tovar@udgvirtual.udg.mx>
2025-03-25 21:33:34 -0600

    Cambiar a serverless para Vercel

--- api/hello.js
+++
@@ -1,5 +0,0 @@
-// api/hello.js
-module.exports = (req, res) => {
-    res.status(200).send("¡Hola, mundo! Tu servidor está funcionando 🚀");
-  };
-  
---
+++ api/index.js
@@ -0,0 +1,3 @@
+export default (req, res) => {
+  res.status(200).send('Esta es la Actividad 3.3: Ephemeral Environments de Tendencias en entorno de desarrollo de aplicaciones y servicios!');
+};
